1dc9d20af662d07f7ed128de37fd7d6b6daad315
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-02-15 16:58:21 +0000

    Added back button function in JS

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,12 +1,48 @@
 "use strict";
 
-var buttons = document.getElementsByClassName("buttons");
+// Variables
+var allButtons = document.getElementsByClassName("buttons");
+var numberButtons = document.getElementsByClassName("buttons__number");
+var clearButton = document.getElementById("clear");
+var backButton = document.getElementById("back");
+var operatorButtons = document.getElementsByClassName("buttons__operator");
+var currentInput = document.getElementById("current-input"); // Functions
+// Adds clicked number to display
 
-var onDayButtonClick = function onDayButtonClick(event) {
-  var day = event.target.innerText;
-  console.log("Button pressed: ".concat(day));
-};
+var onNumberButtonClick = function onNumberButtonClick(event) {
+  var displayedButton = event.target.innerText;
+  currentInput.innerText += displayedButton;
+}; // Adds clicked number to display
 
-for (var index = 0; index < buttons.length; index++) {
-  buttons[index].addEventListener("click", onDayButtonClick);
-}
+
+var onOperatorButtonClick = function onOperatorButtonClick(event) {
+  var displayedButton = event.target.innerText;
+  currentInput.innerText += displayedButton;
+}; // Clears display
+
+
+var onClearButtonClick = function onClearButtonClick(event) {
+  currentInput.innerText = "";
+}; // Clears one character from display
+
+
+var onBackButtonClick = function onBackButtonClick(event) {
+  var displayedButton = event.target.innerText;
+  currentInput.innerText = currentInput.innerText.slice(0, -1);
+}; // Logic
+// Calls onNumberButtonClick function when number is clicked
+
+
+for (var index = 0; index < numberButtons.length; index++) {
+  numberButtons[index].addEventListener("click", onNumberButtonClick);
+} // Calls onOperatorButtonClick function when number is clicked
+
+
+for (var _index = 0; _index < operatorButtons.length; _index++) {
+  operatorButtons[_index].addEventListener("click", onOperatorButtonClick);
+} // Calls onClearButtonClick function when AC is clicked
+
+
+clearButton.addEventListener("click", onClearButtonClick); // Calls onBackButtonClick function when AC is clicked
+
+backButton.addEventListener("click", onBackButtonClick);
--- script.js
+++ script.js
@@ -1,10 +1,55 @@
-const buttons = document.getElementsByClassName("buttons");
+// Variables
+const allButtons = document.getElementsByClassName("buttons");
+const numberButtons = document.getElementsByClassName("buttons__number");
+const clearButton = document.getElementById("clear");
+const backButton = document.getElementById("back");
+const operatorButtons = document.getElementsByClassName("buttons__operator");
+const currentInput = document.getElementById("current-input");
 
-const onDayButtonClick = (event) => {
-  const day = event.target.innerText;
-  console.log(`Button pressed: ${day}`);
+
+// Functions
+
+
+// Adds clicked number to display
+const onNumberButtonClick = (event) => {
+  const displayedButton = event.target.innerText;
+  currentInput.innerText += displayedButton;
 };
 
-for (let index = 0; index < buttons.length; index++) {
-  buttons[index].addEventListener("click", onDayButtonClick);
+// Adds clicked number to display
+const onOperatorButtonClick = (event) => {
+  const displayedButton = event.target.innerText;
+  currentInput.innerText += displayedButton;
+};
+
+// Clears display
+const onClearButtonClick = (event) => {
+  currentInput.innerText = "";
+};
+
+// Clears one character from display
+const onBackButtonClick = (event) => {
+  const displayedButton = event.target.innerText;
+  currentInput.innerText = currentInput.innerText.slice(0,-1); 
+};
+
+
+
+// Logic
+
+
+// Calls onNumberButtonClick function when number is clicked
+for (let index = 0; index < numberButtons.length; index++) {
+  numberButtons[index].addEventListener("click", onNumberButtonClick);
 }
+
+// Calls onOperatorButtonClick function when number is clicked
+for (let index = 0; index < operatorButtons.length; index++) {
+  operatorButtons[index].addEventListener("click", onOperatorButtonClick);
+}
+
+// Calls onClearButtonClick function when AC is clicked
+clearButton.addEventListener("click", onClearButtonClick);
+
+// Calls onBackButtonClick function when AC is clicked
+backButton.addEventListener("click", onBackButtonClick);
